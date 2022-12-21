160c25203aec6da8a6fdc10058101a2f8ea72c33
MurahariRaju <r170714@rguktrv.ac.in>
2022-12-21 20:45:44 +0530

    My To do list project

---
+++ javascript/array.js
@@ -0,0 +1,54 @@
+// Array literal 
+
+const ids =[1,2,3,4,5,6]
+
+// array constructor
+
+// const ids2= new Array(1,2,3,4)
+
+ids[0]=7
+
+// not allowed
+
+// ids =[321.3,4]
+
+// console.log(ids[5])
+// console.log(ids)
+
+//length
+
+// const common =[1,2,"q","e",true,null,1,2]
+// console.log(common.length)
+
+// console.log(common[common.length-1]);
+
+// push
+
+// const user=[
+//     {
+//         id:1,
+//         name:"raju",
+//         password:"123456",
+//     },
+//     {
+//         id:2,
+//         name:"raju 2",
+//         password:"12345",
+//     }
+// ]
+// const name = "Raju Murahari"
+// const nameArr = name.split(" ")
+
+// console.log(name)
+// console.log(nameArr)
+
+// const nameJoined = nameArr.join()
+
+// a1=[12,3,4,5]
+
+// const a=a1.slice()
+
+// Sorting 
+const products =["mobile","laptop","desktop","book"]
+
+const sortedProduct=s= products.sort()
